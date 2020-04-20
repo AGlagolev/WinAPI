ef1574aaa48ef514954ed67397b9591082b13176
Alex <Alex@DESKTOP-CEQG6CC>
2020-04-20 11:31:00 +0300

    FG

---
+++ ModalAndNotNodalWindows/resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDD_MAIN                        101
+#define IDD_MODAL                       103
+#define IDD_DIALOG1                     104
+#define IDD_NMODAL                      104
+#define ID_BTN_MODAL                    1001
+#define ID_BTN_NOT_MODAL                1002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
